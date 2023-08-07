f8efd936bdd8c0adc9792bf0cd94a9381fef1929
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-08-07 01:21:37 -0500

    create skeleton of page

---
+++ src/Components/Atoms/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <h1 className="text-3xl font-bold underline">My Super Cool App</h1>
+      </header>
+      <p className="text-3xl font-bold underline">
+      Hello world!
+    </p>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Molecules/index.js
@@ -0,0 +1,2 @@
+export { default as Header } from './Navbar/Navbar.jsx'
+export { default as Footer } from './Footer/Footer.jsx'
